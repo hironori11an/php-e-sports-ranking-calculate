621e059e14a8f97eada6d1cc6d0d64ffbbf1b736
hironori <hironori11an@gmail.com>
2025-03-19 20:34:20 +0900

    Dockerによる開発環境構築

---
+++ .docker/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 開発環境の場合のみ、Xdebugを有効にする
+if [ "$APP_ENV" = "development" ] &&
+  [ ! -e "/usr/local/etc/php/conf.d/xdebug.ini" ] ; then 
+    pecl install xdebug && \
+    docker-php-ext-enable xdebug
+    # Config XDebug
+    echo "
+        xdebug.mode=debug,coverage
+        xdebug.client_host=host.docker.internal
+        xdebug.client_port=9003
+        xdebug.start_with_request=yes
+        xdebug.discover_client_host=1
+        " > /usr/local/etc/php/conf.d/xdebug.ini;
+fi
+
+exec "$@"
